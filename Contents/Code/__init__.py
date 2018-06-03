2fd31915bab0e7dbb788d04734797ef4e47327a3
Cyt0mand3r <32689564+Cyt0mand3r@users.noreply.github.com>
2018-06-03 02:53:41 -0400

    Update __init__.py
    This fixes the error when viewing a shows episodes

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -193,7 +193,7 @@ def ShowMenu(title, thumb, url):
     dt = [d.strip() for d in html.xpath('//dt[text()="Type:"]/following-sibling::dd/text()') if (d.strip() != ',') and (d.strip() != '')]
     kind = dt[0] if dt else None
 
-    server_node = html.xpath('//div[@data-type="direct"]')
+    server_node = html.xpath('//div[@data-type="iframe"]')
     if not server_node:
         oc.header = 'Warning'
         oc.message = 'There is no episodes for this anime, we will update asap!'
